8e571f7407fb815a84c7342971be879af8faa1a6
ArshithGVaz <arshithvaz630@gmail.com>
2024-07-31 17:33:12 +0530

    Event page

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,7 +1,7 @@
 // src/components/Calendar.jsx
 
 import React, { useState } from "react";
-
+import { events } from "./event";
 // List of months for the dropdown
 const months = [
   "January",
@@ -18,15 +18,8 @@ const months = [
   "December",
 ];
 
-const events = [
-  { date: "2022-01-03", title: "Design review", time: "10AM" },
-  { date: "2022-01-03", title: "Sales meeting", time: "2PM" },
-  { date: "2022-01-07", title: "Date night", time: "6PM" },
-  { date: "2022-01-12", title: "Sam's birthday party", time: "2PM" },
-  { date: "2022-01-22", title: "Maple syrup museum visit", time: "3PM" },
-  { date: "2022-01-22", title: "Hockey game", time: "7PM" },
-  { date: "2022-02-04", title: "Cinema with friends", time: "9PM" },
-];
+
+
 
 const Calendar = () => {
   const [selectedMonth, setSelectedMonth] = useState(0); // Start with January
---
+++ src/components/event.js
@@ -0,0 +1,9 @@
+export const events = [
+    { date: "2022-01-03", title: "Design review", time: "10AM" },
+    { date: "2022-01-03", title: "Sales meeting", time: "2PM" },
+    { date: "2022-01-07", title: "Date night", time: "6PM" },
+    { date: "2022-01-12", title: "Sam's birthday party", time: "2PM" },
+    { date: "2022-01-22", title: "Maple syrup museum visit", time: "3PM" },
+    { date: "2022-01-22", title: "Hockey game", time: "7PM" },
+    { date: "2022-02-04", title: "Cinema with friends", time: "9PM" },
+  ];
